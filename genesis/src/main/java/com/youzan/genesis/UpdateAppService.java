312341067dacf58c866280fd50f2cbd2bfd0e2a6
石翔飞 <francisshi@126.com>
2016-02-15 13:20:31 +0800

    add notif

--- genesis/src/main/java/com/youzan/genesis/UpdateAppService.java
+++ genesis/src/main/java/com/youzan/genesis/UpdateAppService.java
@@ -1,16 +1,15 @@
 package com.youzan.genesis;
 
-import android.app.DownloadManager;
 import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.Uri;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Parcelable;
+import android.support.v4.app.NotificationCompat;
 
 import com.youzan.genesis.info.DownloadInfo;
 import com.youzan.genesis.utils.DownloadUtil;
@@ -30,43 +29,13 @@ public class UpdateAppService extends Service {
     private DownloadInfo downloadInfo;
     private static final int DOWNLOAD_FAIL = -1;
     private static final int DOWNLOAD_SUCCESS = 0;
-    private CompleteReceiver completeReceiver;
-    private long downloadId = 0;
-    private DownloadManager downloadManager;
-
-    class CompleteReceiver extends BroadcastReceiver {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (action.equals(DownloadManager.ACTION_DOWNLOAD_COMPLETE)) {
-                isDownloading = false;
-                long completeDownloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
-                if (completeDownloadId == downloadId) {
-                    Uri uri = downloadManager.getUriForDownloadedFile(downloadId);
-                    Intent installIntent = new Intent(Intent.ACTION_VIEW);
-                    installIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    installIntent.setDataAndType(uri, "application/vnd.android.package-archive");
-                    context.startActivity(installIntent);
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        completeReceiver = new CompleteReceiver();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
-        registerReceiver(completeReceiver, filter);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(completeReceiver);
-    }
+    private static final int NOTIFY_ID = 0xA1;
+    private NotificationManager mNotificationManager = null;
+    private PendingIntent mPendingIntent = null;
+    private String mDownloadProgressStr = null;
+    private Notification mNotification = null;
+    private NotificationCompat.Builder mBuilder = null;
+    private Intent lastIntent;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -100,6 +69,7 @@ public class UpdateAppService extends Service {
             return super.onStartCommand(intent, flags, startId);
         }
 
+        showStartNotification();
         startDownload();
 
         return super.onStartCommand(intent, flags, startId);
@@ -113,6 +83,8 @@ public class UpdateAppService extends Service {
         if (intent == null) {
             return;
         }
+        mDownloadProgressStr = getString(R.string.download_progress);
+        lastIntent = intent;
 
         Parcelable parcelable = intent.getParcelableExtra(ARG_DOWNLOAD_INFO);
         if (parcelable != null && parcelable instanceof DownloadInfo) {
@@ -123,19 +95,76 @@ public class UpdateAppService extends Service {
         }
     }
 
-    private void startDownload() {
+    private void showStartNotification() {
+        if (null == mNotificationManager) {
+            mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
 
+        Intent completingIntent = new Intent();
+        completingIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        completingIntent.setClass(this, UpdateAppService.class);
+        mPendingIntent = PendingIntent.getActivity(UpdateAppService.this, NOTIFY_ID, completingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.drawable.download_icon)
+                .setContentIntent(mPendingIntent)
+                .setWhen(System.currentTimeMillis())
+                .setDefaults(~Notification.DEFAULT_ALL)
+                .setAutoCancel(false)
+                .setOngoing(true)
+                .setContentTitle(getString(R.string.download_start))
+                .setProgress(100, 0, false);
+
+        mNotification = mBuilder.build();
+        mNotification.flags = Notification.FLAG_ONGOING_EVENT;
+
+        mNotificationManager.cancel(NOTIFY_ID);
+        mNotificationManager.notify(NOTIFY_ID, mNotification);
+    }
+
+    private void showUpdateNotification(int progress) {
+        if (mBuilder != null && mNotificationManager != null) {
+            mBuilder.setProgress(100, progress, false)
+                    .setContentTitle(downloadInfo.getFileName())
+                    .setContentText(String.format(mDownloadProgressStr, progress) + "%");
+            mNotification = mBuilder.build();
+            mNotificationManager.notify(NOTIFY_ID, mNotification);
+        }
+    }
+
+    private void showErrorNotification() {
+        if (null == mNotificationManager) {
+            mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        PendingIntent retryIntent = PendingIntent.getService(this, 0,
+                lastIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        builder.setSmallIcon(R.drawable.download_icon)
+                .setContentTitle(apkFile.getName())
+                .setContentText(getString(R.string.download_fail_retry))
+                .setContentIntent(retryIntent)
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setOngoing(false);
+        Notification notification = builder.build();
+        mNotificationManager.cancel(NOTIFY_ID);
+        mNotificationManager.notify(NOTIFY_ID, notification);
+    }
+
+    private void startDownload() {
         isDownloading = true;
         DownloadUtil.newInstance().download(downloadInfo.getDownloadUrl(), apkFile, true, new DownloadUtil.DownloadListener() {
             @Override
             public void downloading(int progress) {
-               showUpdateNotification(progress);
+                showUpdateNotification(progress);
             }
 
             @Override
             public void downloaded() {
                 isDownloading = false;
-
                 showUpdateNotification(100);
 
                 if (apkFile.exists() && apkFile.isFile()
@@ -163,12 +192,6 @@ public class UpdateAppService extends Service {
                 stopSelf();
             }
         });
-
-
-    }
-
-    
-    private void showUpdateNotification(int progress) {
     }
 
     private void handleMessage(Message msg) {
@@ -176,15 +199,11 @@ public class UpdateAppService extends Service {
             case DOWNLOAD_SUCCESS:
                 ToastUtil.show(this, R.string.download_success);
                 FileUtil.install(this, apkFile);
-
-//                if (showNotificationLisenter != null) {
-//                    showNotificationLisenter.showSuccessNotification();
-//                }
                 break;
             case DOWNLOAD_FAIL:
                 ToastUtil.show(this, R.string.download_fail);
                 //重新下载
-                //showFailNotification(lastIntent);
+                showErrorNotification();
                 break;
             default:
                 break;
